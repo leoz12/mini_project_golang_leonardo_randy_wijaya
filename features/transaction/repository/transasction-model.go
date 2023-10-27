da937d38870cdc4879209ea1feb6e9a3a5de3e58
Leo <lnardo833@gmail.com>
2023-10-27 22:16:59 +0700

    add crud transasction

---
+++ features/transaction/repository/transasction-model.go
@@ -0,0 +1,23 @@
+package transactionRepository
+
+import (
+	userRepository "mini_project/features/user/repository"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Transaction struct {
+	ID              string `gorm:"primarykey"`
+	UserId          string `gorm:"size:191"`
+	User            userRepository.User
+	GameId          string
+	GameName        string
+	GameDescription string
+	Quantity        int
+	Price           float32
+	Discount        float32
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	DeletedAt       gorm.DeletedAt `gorm:"index"`
+}
